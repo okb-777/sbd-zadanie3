7294502e5db0fbd7656e7ce5c3f7ffe0a50a0b7a
okb-777 <bartekkszke2@gmail.com>
2022-11-02 10:01:57 +0100

    zadanie

--- zapytania.sql
+++ zapytania.sql
@@ -6,3 +6,12 @@ SELECT * FROM `customers` WHERE NOT country = "Germany" AND NOT country = "USA";
 SELECT * FROM `customers` ORDER BY country ASC;
 SELECT * FROM `customers` ORDER BY country DESC;
 SELECT * FROM `customers` ORDER BY country DESC, customer_name ASC;
+
+SELECT MIN(price) FROM `products`;
+SELECT MAX(price) FROM `products`;
+SELECT COUNT(*) FROM `products`;
+SELECT COUNT(*) FROM `customers`;
+SELECT country, COUNT(*) FROM `customers` GROUP BY country;
+SELECT COUNT(category_id), AVG(price) FROM `products` WHERE category_id=1;
+SELECT SUM(price) FROM `products` WHERE category_id=2;
+SELECT category_id, MAX(price) FROM `products`WHERE category_id=1 OR category_id=2 GROUP BY category_id;
