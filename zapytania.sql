de13275a4ef1c8c454d60ff91f13bbc9acf5c6ea
okb-777 <bartekkszke2@gmail.com>
2022-11-02 12:06:32 +0100

    zadanie

--- zapytania.sql
+++ zapytania.sql
@@ -17,3 +17,16 @@ SELECT country, COUNT(*) FROM `customers` GROUP BY country;
 SELECT COUNT(category_id), AVG(price) FROM `products` WHERE category_id=1;
 SELECT SUM(price) FROM `products` WHERE category_id=2;
 SELECT category_id, MAX(price) FROM `products` WHERE category_id=1 OR category_id=2 GROUP BY category_id;
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+SELECT * FROM `products` WHERE price BETWEEN 10 AND 20;
+SELECT * FROM `products` WHERE price NOT BETWEEN 10 AND 20;
+SELECT * FROM `products` WHERE category_id IN (1,2,3);
+SELECT * FROM `orders` WHERE shipper_id IN (1,2,3);
+SELECT * FROM `products` WHERE (price BETWEEN 10 AND 15) AND (category_id NOT BETWEEN 1 AND 4);
+SELECT * FROM `orders` WHERE order_date BETWEEN CAST('19960701' as date) AND CAST('19960731' as date);
+SELECT * FROM `orders` WHERE customer_id IN( SELECT id FROM customers WHERE id BETWEEN 77 AND 90 );
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
