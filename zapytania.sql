864fcbec3dfa87c70016beaebfe33dbd3660a4d7
okb-777 <113498827+okb-777@users.noreply.github.com>
2022-11-17 14:44:51 +0100

    zadanie

--- zapytania.sql
+++ zapytania.sql
@@ -51,3 +51,31 @@ SELECT * FROM `orders` LIMIT 10 OFFSET 10;
 SELECT * FROM `orders` ORDER BY id DESC LIMIT 10;
 
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#HAVING , EXISTS
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+SELECT country FROM `customers` GROUP BY country HAVING COUNT(country)>5;
+SELECT * FROM `customers` WHERE EXISTS (SELECT * FROM orders WHERE customers.id = orders.customer_id HAVING COUNT(orders.customer_id) BETWEEN 2 AND 4) ORDER BY customers.id DESC;
+SELECT * FROM `employees` WHERE EXISTS (SELECT * FROM orders WHERE employees.id = orders.employee_id HAVING COUNT(orders.employee_id)>10);
+SELECT * FROM `employees` WHERE last_name="Davolio" OR last_name="Fuller" AND EXISTS (SELECT * FROM orders WHERE employees.id = orders.employee_id HAVING COUNT(orders.employee_id)>25);
+SELECT * FROM `suppliers` WHERE EXISTS (SELECT * FROM products WHERE suppliers.id = products.supplier_id AND products.price<20);
+SELECT * FROM `suppliers` WHERE EXISTS (SELECT * FROM products WHERE suppliers.id = products.supplier_id AND products.price=22);
+
+#CASE
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+SELECT products.name, CASE WHEN orders_details.quantity > 30 THEN "Liczba sztuk jest większa niż 30" WHEN orders_details.quantity = 30 THEN "Liczba sztuk jest równa 30" ELSE "Liczba sztuk jest mniejsza niż 30" END AS liczba_sztuk FROM products JOIN orders_details ON products.id = orders_details.product_id;
+SELECT * FROM `customers` ORDER BY (CASE WHEN city IS NULL THEN country ELSE city END);
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#ALL
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+SELECT products.id, products.name, SUM(orders_details.quantity) AS ilość FROM `products` JOIN orders_details ON products.id = orders_details.product_id GROUP BY(products.name) ORDER BY `ilość` DESC LIMIT 10;
+SELECT customers.id, customers.customer_name, COUNT(orders.customer_id) AS liczba_zamówień FROM `customers` JOIN orders ON customers.id = orders.customer_id GROUP BY (customers.customer_name) ORDER BY `liczba_zamówień` DESC LIMIT 10;
+SELECT categories.id, categories.category_name, COUNT(products.category_id) AS ilość FROM `categories` JOIN products ON categories.id = products.category_id GROUP BY categories.category_name ORDER BY ilość DESC LIMIT 1;
+
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
